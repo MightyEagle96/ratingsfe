daa091e4b6627f0d18604dfcf3acb079673b8e52
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-09-24 08:40:44 +0100

    made changs

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,6 +8,17 @@ import { Spinner } from "react-bootstrap";
 function HomePage() {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(false);
+  const [closeRegistration, setCloseRegistration] = useState(false);
+
+  const closeTime = new Date(2022, 8, 24, 9, 0, 0);
+  const int = setInterval(() => {
+    if (Date.now() >= Date.parse(closeTime)) {
+      setCloseRegistration(true);
+
+      clearInterval(int);
+    }
+  }, 1000);
+
   const handleChange = (e) =>
     setData({ ...data, [e.target.name]: e.target.value });
 
@@ -41,6 +52,69 @@ function HomePage() {
       <div className="container mt-3">
         <div className="d-none d-md-block">
           <div className="d-flex justify-content-center ">
+            {closeRegistration ? (
+              <Typography fontFamily={"Anton"} variant="h4">
+                VOTING HAS ENDED
+              </Typography>
+            ) : (
+              <div className="col-md-4">
+                <Typography
+                  fontFamily={"Lato"}
+                  color="GrayText"
+                  fontWeight={600}
+                  textAlign={"center"}
+                  gutterBottom
+                >
+                  Enter your name and phone number
+                </Typography>
+
+                <div className="mb-3">
+                  <TextField
+                    fullWidth
+                    label="Name"
+                    name="name"
+                    onChange={handleChange}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <Person />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <TextField
+                    fullWidth
+                    label="Phone Number"
+                    name="phoneNumber"
+                    type="number"
+                    onChange={handleChange}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <Phone />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </div>
+                <div className="mt-3">
+                  <Button fullWidth variant="contained" onClick={register}>
+                    {loading ? <Spinner animation="grow" /> : "Register"}
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="d-sm-block d-md-none">
+          {closeRegistration ? (
+            <Typography fontFamily={"Anton"} variant="h4">
+              VOTING HAS ENDED
+            </Typography>
+          ) : (
             <div className="col-md-4">
               <Typography
                 fontFamily={"Lato"}
@@ -90,58 +164,7 @@ function HomePage() {
                 </Button>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="d-sm-block d-md-none">
-          <div className="col-md-4">
-            <Typography
-              fontFamily={"Lato"}
-              color="GrayText"
-              fontWeight={600}
-              textAlign={"center"}
-              gutterBottom
-            >
-              Enter your name and phone number
-            </Typography>
-
-            <div className="mb-3">
-              <TextField
-                fullWidth
-                label="Name"
-                name="name"
-                onChange={handleChange}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Person />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </div>
-
-            <div className="mb-3">
-              <TextField
-                fullWidth
-                label="Phone Number"
-                name="phoneNumber"
-                type="number"
-                onChange={handleChange}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Phone />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </div>
-            <div className="mt-3">
-              <Button fullWidth variant="contained" onClick={register}>
-                {loading ? <Spinner animation="grow" /> : "Register"}
-              </Button>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
